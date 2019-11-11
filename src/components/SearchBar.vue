bb3becb95756aa32f5073cc0ff5546486a7b6694
ll125498a <caoxuan2011@hotmail.com>
2019-11-11 18:14:50 +0800

    GET

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,41 @@
 <template>
 <div id="searchBox">
-<form id="searchCheck">
-    <el-input id="search" v-model="Search" placeholder="请输入搜索内容"></el-input>
-</form>
+
+    <el-form id="searchCheck">
+    <!-- <el-input id="search" v-model="Search" placeholder="请输入搜索内容" >     
+    </el-input> -->
+    <el-autocomplete id="search" v-model="Contents" placeholder="请输入搜索内容" :fetch-suggestions="querySearch" @select="handleSelect" :trigger-on-focus="false">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-autocomplete>
+            </el-form>
+    
+
 </div>
 </template>
-<script>
+<script>  
 export default {
     data(){
         return{
-            Search:''
+            Contents:''
+        }
+    },
+    methods:{
+        querySearch(querystring,callback)
+        {
+            var Contents=this.Contents
+            var results="https://www.baidu.com/s?wd="+querystring
+            axios.get(results).then((response)=>{
+                for(let i of response.data)
+                {  
+                    i.value=i.data
+                }
+            })
+        },
+        handleSelect(){
+            
         }
     }
+
     
 }
 </script>
